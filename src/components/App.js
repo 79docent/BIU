7ff2d57dee1bda8fcd02122a64623c8c17fea039
sjakobiak <s20574@pjwstk.edu.pl>
2022-01-30 00:36:57 +0100

    added MovieList components, now we are able to see movies on the page

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Nav from './Nav';
 import SearchArea from "./SearchArea";
+import MovieList from "./MovieList";
 
 class App extends Component {
 
@@ -34,6 +35,7 @@ class App extends Component {
       <div className="App">
         <Nav></Nav> 
         <SearchArea handleSubmit={this.handleSubmit} handleChange={this.handleChange}></SearchArea>
+        <MovieList movies={this.state.movies}></MovieList>
       </div>
     );
   }
---
+++ src/components/Movie.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Movie = (props) => {
+    return (
+        <div className="col s12 m6 l3">
+            <div className="card">
+                <div className="card-image waves-effect waves-block waves-light">
+                    {
+                        props.image == null ? <img src={`https://s3-ap-southeast-1.amazonawscom/upcode/static/default-image.jpg`} alt="default image" style={{ width: "100%", height: 360 }} /> : 
+                        <img src={`https://image.tmdb.org/t/p/w500/${props.image}`} alt="card image" style={{ width: "100%", height: 360 }} /> 
+                    }
+                </div>     
+
+                <div className="card-content">
+                    <p><a href="#">View Details</a></p>
+                </div> 
+
+            </div>
+        </div>
+    )
+}
+
+export default Movie;
