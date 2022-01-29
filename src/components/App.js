2f2bd08a097bf4bbda626cd20cf0e72402010fbc
sjakobiak <s20574@pjwstk.edu.pl>
2022-01-30 00:06:34 +0100

    added MoviesDB API and implemented first fetch

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,43 @@
-import React from "react";
+import React, { Component } from "react";
 import Nav from './Nav';
 import SearchArea from "./SearchArea";
 
-function App() {
-  return (
-    <div className="App">
-      <Nav></Nav> 
-      <SearchArea></SearchArea>
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      movies: [],
+      searchTerm: ''
+    }
+    this.apiKey = process.env.REACT_APP_API
+  }
+
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    fetch(`https://api.themoviedb.org/3/search/movie?api_key=${this.apiKey}&query=${this.state.searchTerm}`)
+    .then(data => data.json())
+    .then(data => {
+      console.log(data);
+      this.setState({ movies: [...data.results]})
+    })
+  }
+
+  handleChange = (e) => {
+    this.setState({ searchTerm: e.target.value })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Nav></Nav> 
+        <SearchArea handleSubmit={this.handleSubmit} handleChange={this.handleChange}></SearchArea>
+      </div>
+    );
+  }
 }
+
 
 export default App;
